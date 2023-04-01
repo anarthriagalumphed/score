64827e687ff2ae49c5db5227570cab0a6f7e07b8
anarthriagalumphed <galih22.mp@gmail.com>
2023-03-31 23:54:12 -0700

    score update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { MATCH } from '../../assets/data';
+import { CommonModule } from '@angular/common';
+import { prependOnceListener } from 'process';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, CommonModule]
 })
 export class Tab2Page {
 
   constructor() {}
-
   public match = MATCH;
 
 }
